655351b339a0771cd18d878f75a319f3778a0f75
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-08-06 11:50:08 +0300

    Added apps from labs and exercises.

--- 01. Introduction to ASP.NET Core/Panda/Panda.Domain/Models/PandaRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Panda.Domain.Models
-{
-    public class PandaRole : IdentityRole
-    {
-    }
-}
--- 02. Introduction to Razor - Views and Layouts/Chushka/Data/Models/Enums/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Chushka.Data.Models.Enums
-{
-    public enum Role
-    {
-        User = 0,
-        Admin = 1
-    }
-}
